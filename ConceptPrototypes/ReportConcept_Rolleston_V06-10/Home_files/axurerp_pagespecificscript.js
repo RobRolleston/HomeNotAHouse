ac4a19f56bc47d92d735b3d45758f749d41bc413
Rob Rolleston <rrolleston@mica.edu>
2013-06-10 10:18:47 -0400

    Add Report Concept for simple 2-attribute initial system
    Open the index.html with FF or Chrome.  The original Axure .rp file is
    also there

---
+++ ConceptPrototypes/ReportConcept_Rolleston_V06-10/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,27 @@
+﻿for(var i = 0; i < 6; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('V2013_05_27.html');
+
+}
+});
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('V2013_06_10.html');
+
+}
+});
+gv_vAlignTable['u0'] = 'top';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u3'] = 'top';
---
+++ ConceptPrototypes/ReportConcept_Rolleston_V06-10/V2013_06_10_files/axurerp_pagespecificscript.js
@@ -0,0 +1,60 @@
+﻿for(var i = 0; i < 107; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u90'] = 'center';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u23'] = 'top';document.getElementById('u1_img').tabIndex = 0;
+
+u1.style.cursor = 'pointer';
+$axure.eventManager.click('u1', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+	SetPanelState('u26', 'pd2u26','none','',500,'none','',500);
+
+	SetPanelState('u57', 'pd2u57','none','',500,'none','',500);
+
+	SetPanelState('u88', 'pd2u88','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u104'] = 'center';gv_vAlignTable['u106'] = 'center';gv_vAlignTable['u94'] = 'center';gv_vAlignTable['u100'] = 'center';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u92'] = 'center';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u0'] = 'top';
+u3.style.cursor = 'pointer';
+$axure.eventManager.click('u3', function(e) {
+
+if (true) {
+
+	SetPanelState('u26', 'pd0u26','none','',500,'none','',500);
+
+	SetPanelState('u57', 'pd0u57','none','',500,'none','',500);
+
+	SetPanelState('u88', 'pd0u88','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u102'] = 'center';gv_vAlignTable['u98'] = 'center';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u96'] = 'center';
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	SetPanelState('u26', 'pd1u26','none','',500,'none','',500);
+
+	SetPanelState('u57', 'pd1u57','none','',500,'none','',500);
+
+	SetPanelState('u88', 'pd1u88','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u4'] = 'top';
