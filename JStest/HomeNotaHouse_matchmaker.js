38bb55a58af7847ea539d3e5cdc5ab26a5ec647a
Annie Marcotte <amarcotte@mica.edu>
2013-06-27 00:20:24 -0400

    maturing algorithm

--- JStest/HomeNotaHouse_matchmaker.js
+++ JStest/HomeNotaHouse_matchmaker.js
@@ -8,7 +8,6 @@ $.getJSON( "CSAdata_sales-crime.json", function(data){
 });
 
 
-//var matches = new Array();
 
 // This function is called by the "submit" action in jstest.html
 function findNeighborhood(pricevalues){
@@ -17,19 +16,21 @@ function findNeighborhood(pricevalues){
 	console.log(pricevalues);
 	
 	// make the findPriceValue function a variable
-	var neighborhoodPrice = findPriceValue();
+	var neighborhoodPrice = findPriceValue(pricevalues);
 	
 	//show that we have access to the data
-	$("h1").html(neighborhoodPrice);
+	$("ul").html("<li>" + neighborhoodPrice + "</li>");
 	//now compare/combine to your heart's content!
 
 };
 
 
-// This displays the neighborhood
+// sets up a new array to put the new matches
+var matchList = new Array();
+
+// This determines if the neighborhood is a match for the inputted price range
 function findPriceValue(pricevalues) {
 	
-	var Matches = new Array();
 	// cycle through all neighborhoods
 	for (i = 0; i < csaData.length; i++) {
 		
@@ -37,79 +38,26 @@ function findPriceValue(pricevalues) {
 		var medianPrice = csaData[i].salepr10;
 		
 		// Check to see if they fit the price range from user input
-		if ( 0 < medianPrice && medianPrice < 100000 ) {
-			console.log('match made!');
+		if ( pricevalues[0] < medianPrice && medianPrice < pricevalues[1] ) {
+			matchList.push(i);
 		} else {
 			console.log('no match!');
 		}
 	
 	}
-
-	return csaData[0].CSA2000 + ': ' + csaData[0].salepr00;
+	
+	var Matches = displayMatches(matchList);
+	return Matches;
 };
 
-
-
-
-/*
-// One big function to run all the matching and scoring
-function  findNeighborhood (pricevalues) {
+// Displays the sales price of each
+function displayMatches(matchList) {
 	
-	var pricevalues = stores the housing price value selected by the user
-	var crimevalues = stores the crime rate value selected by the user
+	for (i = 0; i < matchList.length; i++) {
+		csaData[matchList[i]].salepr10;
+	}
 	
-	for every neighborhood_name in neighborhoods {
-	
-		// Checks if any of the neighborhood crime rates match the selected range
-		if crime_rate == selectedCrime {
-			crime_match == true
-		} else {
-			crime_match == false
-		};
-		
-		// Check if any of the neighborhood house prices match the selected range
-		if house_price {
-			house_price == true
-		} else {
-			house_price == false
-		};
-		
-		// Checks if any of the neighborhoods have both a price match and crime match
-		if crime_rate, house_price == true {
-			write.html("the perfect match! (or something)")
-		};
-		
-		// Assign a score to each neighborhood. Thanks Rob!
-		var hoodScore = SQRT( (price_desired - price_neigh)^2 + (crime_desired - crime_neigh)^2 )
-	
-	};
-	
-	// Display the results by writing HTML
-	document.GetElementbyID("neighborhood_profile").innerHTML {
-		// This puts whatever HTML element we need to display the results
-		// I have no idea what I'm doing here. I want it to select three lowest scores.
-		neighborhoods.hoodScore[0]
-		neighobrhoods.hoodScore[1]
-		neighborhoods.hoodScore[2]
-	};
-
-}
-
-// Another function to create visualizations of trends, using D3. THIS IS NOT FINISHED YET
-d3.json("csa2010.json", function(json) {
-
-	// turns strings into number values
-	json.forEach(function(d) {
-        d.wordcount = +d.wordcount
-    });
-
-});
-*/
+	//console.log(csaData[Matches[i]].salepr10)
+}; 
 
 
-
-// Comments by Anthony: You have two different sections to the algorithm, first just checking if a neighborhood is a match, and then a more precise way to measure and sort them. The second option is more powerful and might be the right thing to do from the beginning. With it, if there are many matches, you can return only the best, and if there are few matches still return the best, even if they are not perfect.
-
-// Consider just having one loop through all of the neighborhoods in which you test if it the neighborhoods are within the ranges and assign true/false values in each based on if they are within the ranges. Then also assign a score to each one based on some combination* of the distance from the range for each metric (as you have done in the second half). Then we can sort that array and return the best options.
-
-// *Think about how multiple metrics can be combined into a single score. What makes a better neighborhood, one metric that's very bad, or two that are ok. This is also something that will be easier to experiment with once we have some code written so we can actually test results very quickly. 
