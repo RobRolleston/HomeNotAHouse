26233af37c839113f9f971411f0267a8f5bee4cc
RobRolleston <rrolleston@mica.edu>
2013-07-28 21:39:31 -0400

    Pass matches to reports page & read names from JSON
    Passing the matches to the reports page.  All that is being filled in
    now are the tab labels

--- End2EndPagesCode/scripts/matchmaker.js
+++ End2EndPagesCode/scripts/matchmaker.js
@@ -3,17 +3,18 @@ console.log("matchmaker.js is on!");
 //Global variable for storing the data (can be seen/edited anywhere)
 var csaData = null;
 //Call to read the json data, and on success populate csaData as an actual javascript object of the data
-$.getJSON( "data/CSAdata_sales-crime.json", function(data){
-	csaData = data;
-	 console.log("CSA Data Loaded");
+$.getJSON( "data/CSAdata_sales-crime.json", 
+	function(data){
+		csaData = data;
+	 	console.log("CSA Data Loaded");
 	
-	// Sort csaData by sale price, ascending order, that way any returned matches will already be sorted
-	csaData.sort(function(a, b) {
-    	return a.salepr10 - b.salepr10;
-	});
-	//console.log(csaData[0], csaData[1], csaData[2]);
+		// Sort csaData by sale price, ascending order, that way any returned matches will already be sorted
+		csaData.sort(function(a, b) {
+    		return a.salepr10 - b.salepr10;
+			});
+		//console.log(csaData[0], csaData[1], csaData[2]);
 	
-}).fail(function() { console.log( "error loading CSA data" ); });
+		}).fail(function() { console.log( "error loading CSA data" ); });
 
 
 // This function is called by the "submit" action in jstest.html
--- End2EndPagesCode/scripts/report.js
+++ End2EndPagesCode/scripts/report.js
@@ -1,21 +1,7 @@
 
 var csaData = null;
 	//Call to read the json data, and on success populate csaData as an actual javascript object of the data
-$.getJSON( "data/CSAdata_sales-crime.json", function(data){
-	csaData = data;
-	console.log("CSA Data Loaded");
-	
-	// Sort csaData by sale price, ascending order, that way any returned matches will already be sorted
-	csaData.sort(function(a, b) {
-    	return a.salepr10 - b.salepr10;
-	});
-	//console.log(csaData[0], csaData[1], csaData[2]);
-	
-}).fail(function() { console.log( "error loading CSA data" ); });
-
-
-//$(function() {
-	
+function parseCache () {	
 	console.log("Passed hash value: " + location.hash);
 	var idx = location.hash.indexOf("#");
 	var matchList = location.hash.slice(idx+1).split(",");
@@ -27,7 +13,28 @@ $.getJSON( "data/CSAdata_sales-crime.json", function(data){
 			//  Howe get the JSON file to load before doing this... ?
 			console.log("Neighborhood #: " + matchList[i] );
 		}
-		 
-	}
+		var tabId = "#tab"+i+"Label";
+		$(tabId).html(csaData[matchList[i]].CSA2000); 
+	};
+};
+	
+$.getJSON( "data/CSAdata_sales-crime.json", 
+	function(data){
+		csaData = data;
+		console.log("CSA Data Loaded");
+	
+		// Sort csaData by sale price, ascending order, that way any returned matches will already be sorted
+		csaData.sort(function(a, b) {
+    		return a.salepr10 - b.salepr10;
+			});
+		//console.log(csaData[0], csaData[1], csaData[2]);
+	
+	})
+	.fail(function() { console.log( "error loading CSA data" ); })
+	.done(function() { parseCache();});
+
+
+	
+
 	
 	
